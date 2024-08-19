94f683127dfd2b422c33f3170c404c42b3e1d392
jewelcodes <omarmelghoul01@gmail.com>
2024-08-19 19:50:01 -0400

    font: added kernel-level font

---
+++ src/include/font.h
@@ -0,0 +1,22 @@
+/*
+ * lux - a lightweight unix-like operating system
+ * Omar Elghoul, 2024
+ * 
+ * Core Microkernel
+ */
+
+/*
+ * This is the Modern DOS font from https://github.com/susam/pcface/, which is
+ * used and redistributed under the Creative Commons License.
+ */
+
+#pragma once
+
+#include <stdint.h>
+
+#define FONT_WIDTH              8
+#define FONT_HEIGHT             16
+#define FONT_MIN_GLYPH          32      // ' '
+#define FONT_MAX_GLYPH          126     // '~'
+
+const uint8_t font[];
