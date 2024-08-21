1740c96114b1ca2625bc0835a1bd41a667ba4d8a
jewelcodes <omarmelghoul01@gmail.com>
2024-08-21 18:59:12 -0400

    x86_64: IDT structure

---
+++ src/platform/x86_64/include/platform/idt.h
@@ -0,0 +1,32 @@
+/*
+ * lux - a lightweight unix-like operating system
+ * Omar Elghoul, 2024
+ * 
+ * Platform-Specific Code for x86_64
+ */
+
+#include <stdint.h>
+
+#define IDT_FLAGS_VALID                 0x8000
+
+#define IDT_FLAGS_TYPE_INTERRUPT        0x0E
+#define IDT_FLAGS_TYPE_TRAP             0x0F
+#define IDT_FLAGS_TYPE_SHIFT            8
+
+#define IDT_FLAGS_DPL_KERNEL            0
+#define IDT_FLAGS_DPL_USER              3
+#define IDT_FLAGS_DPL_SHIFT             13
+
+typedef struct {
+    uint16_t offset_lo;
+    uint16_t segment;
+    uint16_t flags;
+    uint16_t offset_mi;
+    uint32_t offset_hi;
+    uint32_t reserved;
+} __attribute__((packed)) IDTEntry;
+
+typedef struct {
+    uint16_t limit;
+    uint64_t base;
+} __attribute__((packed)) IDTR;
