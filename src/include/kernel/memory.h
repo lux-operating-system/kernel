95c34c62b1b9310d162a63ef0eaf80b1be992987
jewelcodes <omarmelghoul01@gmail.com>
2024-08-21 18:44:40 -0400

    vmm: skeleton for virtual memory manager

--- src/include/kernel/memory.h
+++ src/include/kernel/memory.h
@@ -15,6 +15,10 @@
 
 #define PMM_CONTIGUOUS_LOW      0x01
 
+#define VMM_USER                0x01        // kernel-user toggle
+#define VMM_EXEC                0x02
+#define VMM_WRITE               0x04
+
 typedef struct {
     uint64_t highestPhysicalAddress;
     uint64_t lowestUsableAddress;
@@ -30,3 +34,7 @@ uintptr_t pmmAllocate(void);
 uintptr_t pmmAllocateContiguous(size_t, int);
 int pmmFree(uintptr_t);
 int pmmFreeContiguous(uintptr_t, size_t);
+
+void vmmInit();
+uintptr_t vmmAllocate(size_t, int);
+int vmmFree(uintptr_t, size_t);
--- src/include/platform/platform.h
+++ src/include/platform/platform.h
@@ -9,4 +9,19 @@
 
 #include <stdint.h>
 
-int platformCPUSetup();     // setup one CPU; repeatedly called in multiprocessing systems
+/* routines that must be implemented by any platform-specific code, abstracting
+ * the difference between different platforms; the goal is to keep the core
+ * kernel portable across CPU architectures */
+
+#define PLATFORM_PAGE_PRESENT               0x0001      // present in main memory
+#define PLATFORM_PAGE_SWAP                  0x0002      // present in storage device
+#define PLATFORM_PAGE_USER                  0x0004      // user-kernel toggle
+#define PLATFORM_PAGE_EXEC                  0x0008
+#define PLATFORM_PAGE_WRITE                 0x0010
+#define PLATFORM_PAGE_ERROR                 0x8000
+
+int platformCPUSetup();         // very early setup for one CPU
+int platformPagingSetup();      // paging setup for virtual memory management
+uintptr_t platformGetPageAddress(int *, uintptr_t);     // get physical address and flags of a page
+int platformMapPage(uintptr_t, uintptr_t, int);         // map a physical address to a virtual address
+int platformUnmapPage(uintptr_t);               // and vice versa
