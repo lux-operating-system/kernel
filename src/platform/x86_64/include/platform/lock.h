7709845102b75f43d7206fbc3aa9a5182165f9c9
jewelcodes <omarmelghoul01@gmail.com>
2024-08-24 15:27:18 -0400

    x86_64: implemented locking

---
+++ src/platform/x86_64/include/platform/lock.h
@@ -0,0 +1,17 @@
+/*
+ * lux - a lightweight unix-like operating system
+ * Omar Elghoul, 2024
+ * 
+ * Platform-Specific Code for x86_64
+ */
+
+#pragma once
+
+#include <stdint.h>
+
+/* every platform must provide some implementation of locks */
+typedef uint64_t lock_t;
+int lockStatus(lock_t *);
+int acquireLock(lock_t *);
+int acquireLockBlocking(lock_t *);
+int releaseLock(lock_t *);
---
+++ src/platform/x86_64/lock.asm
@@ -0,0 +1,73 @@
+
+; lux - a lightweight unix-like operating system
+; Omar Elghoul, 2024
+
+[bits 64]
+
+section .text
+
+; Implementation of Locks
+
+; int lockStatus(lock_t *lock)
+; returns zero if the lock is free, one if not
+global lockStatus
+align 16
+lockStatus:
+    xor rax, rax
+    mov al, byte[rdi]
+    and al, al
+    jz .done
+    mov al, 1
+
+.done:
+    ret
+
+; int acquireLock(lock_t *lock)
+; non-blocking function to acquire a lock
+; returns zero if not acquired, one if acquired
+global acquireLock
+align 16
+acquireLock:
+    call lockStatus
+    and rax, rax
+    jnz .fail           ; lock is busy
+
+    lock or byte[rdi], 1
+    call lockStatus
+    jz .fail
+
+    mov rax, 1
+    ret
+
+.fail:
+    xor rax, rax
+    ret
+
+; int acquireLockBlocking(lock_t *lock)
+; blocking function to acquire lock, always returns one
+global acquireLockBlocking
+align 16
+acquireLockBlocking:
+    call lockStatus
+    and rax, rax
+    jnz .wait               ; lock is busy
+
+    lock or byte[rdi], 1
+    call lockStatus
+    jz .wait
+
+    mov rax, 1
+    ret
+
+.wait:
+    pause
+    jmp acquireLockBlocking
+
+; int releaseLock(lock_t *lock)
+; releases a lock, always returns zero
+global releaseLock
+align 16
+releaseLock:
+    lock and byte[rdi], 0
+    mov rax, 0
+    ret
