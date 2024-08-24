11a91f3fe97b62dc94316060024b4eebae1c0489
jewelcodes <omarmelghoul01@gmail.com>
2024-08-24 15:33:59 -0400

    x86_64: lock initial value

--- src/platform/x86_64/include/platform/lock.h
+++ src/platform/x86_64/include/platform/lock.h
@@ -10,6 +10,8 @@
 #include <stdint.h>
 
 /* every platform must provide some implementation of locks */
+#define LOCK_INITIAL            0       // initial free value
+
 typedef uint64_t lock_t;
 int lockStatus(lock_t *);
 int acquireLock(lock_t *);
