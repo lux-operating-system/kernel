e34c79b3dbb3b873edafb710f8ff83e68d750f0e
jewelcodes <omarmelghoul01@gmail.com>
2024-08-26 19:05:28 -0400

    sched: skeleton for scheduler

---
+++ src/include/kernel/sched.h
@@ -0,0 +1,48 @@
+/*
+ * lux - a lightweight unix-like operating system
+ * Omar Elghoul, 2024
+ * 
+ * Core Microkernel
+ */
+
+#pragma once
+
+#include <stdint.h>
+#include <stddef.h>
+
+// this gives us a maximum of over a million running processes
+#define MAX_PID                 0xFFFFF
+
+#define THREAD_QUEUED           0
+#define THREAD_RUNNING          1
+#define THREAD_BLOCKED          2   // waiting for I/O
+
+typedef uint32_t pid_t;
+typedef uint32_t uid_t;
+typedef uint32_t gid_t;
+
+typedef struct {
+    int status;
+    pid_t pid, tid;         // pid == tid for the main thread
+    uint64_t time;
+
+    void *context;          // platform-specific (page tables, registers, etc)
+} Thread;
+
+typedef struct Process {
+    pid_t pid, parent;
+    uid_t user;
+    gid_t group;
+
+    char *env;              // environmental variables
+    char *command;          // command line with arguments
+
+    size_t threadCount;
+    size_t childrenCount;
+
+    Thread **threads;
+    struct Process **children;  // array of pointers of size childrenCount
+} Process;
+
+void schedInit();
+void schedCycle();
