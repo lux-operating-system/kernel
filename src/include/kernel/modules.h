f1ca835357d0dfd396259081f57f4aa8518491fe
jewelcodes <omarmelghoul01@gmail.com>
2024-08-28 20:52:43 -0400

    modules: query and load modules into memory

--- src/include/kernel/modules.h
+++ src/include/kernel/modules.h
@@ -8,9 +8,14 @@
 #pragma once
 
 #include <stdint.h>
+#include <stddef.h>
 #include <kernel/boot.h>
 
 #define MAX_MODULES         16
 
 void modulesInit(KernelBootInfo *);
+int moduleCount();
+size_t moduleQuery(const char *);
+void *moduleLoad(void *, const char *);
+
 void ramdiskInit(KernelBootInfo *);
--- src/modules/modules.c
+++ src/modules/modules.c
@@ -34,3 +34,63 @@ void modulesInit(KernelBootInfo *boot) {
         }
     }
 }
+
+/* moduleCount(): returns the module count
+ * params: none
+ * returns: number of boot modules loaded
+ */
+
+int moduleCount() {
+    int n = 0;
+    for(int i = 0; i < MAX_MODULES; i++) {
+        if(moduleSizes[i] && modules[i]) n++;
+    }
+
+    return n;
+}
+
+/* moduleFind(): finds a module by name
+ * params: name - name of the module
+ * returns: index to the model, -1 on fail
+ */
+
+int moduleFind(const char *name) {
+    int c = moduleCount();
+
+    for(int i = 0; i < c; i++) {
+        if(modules[i] && !strcmp((const char *)modules[i], name)) {
+            return i;
+        }
+    }
+
+    return -1;
+}
+
+/* moduleQuery(): queries the size and existence of a module
+ * params: name - name of the module
+ * returns: size of the module in bytes, zero if it doesn't exist
+ */
+
+size_t moduleQuery(const char *name) {
+    int i = moduleFind(name);
+    if(i < 0) return 0;
+
+    return (size_t) moduleSizes[i];
+}
+
+/* moduleLoad(): loads a module into memory 
+ * params: buffer - buffer to load the module, at least size moduleQuery()
+ * params: name - name of the module
+ * returns: pointer to the buffer, NULL on failure
+ */
+
+void *moduleLoad(void *buffer, const char *name) {
+    int i = moduleFind(name);
+    if(i < 0) return NULL;
+
+    uint8_t *ptr = modules[i];
+    ptr += strlen((const char *)ptr) + 1;
+
+    if(!moduleSizes[i]) return NULL;
+    return memcpy(buffer, ptr, moduleSizes[i]);
+}
