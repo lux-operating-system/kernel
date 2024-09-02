f39d70bde158f84a94d4503cc0876255649a95d3
jewelcodes <omarmelghoul01@gmail.com>
2024-09-02 19:29:11 -0400

    sched: fork() implementation

---
+++ src/sched/fork.c
@@ -0,0 +1,78 @@
+/*
+ * lux - a lightweight unix-like operating system
+ * Omar Elghoul, 2024
+ * 
+ * Core Microkernel
+ */
+
+#include <stdlib.h>
+#include <platform/platform.h>
+#include <platform/context.h>
+#include <kernel/sched.h>
+#include <kernel/logger.h>
+
+/* fork(): forks the running thread
+ * params: t - pointer to thread structure
+ * returns: zero to child, PID of child to parent, negative on fail
+ */
+
+pid_t fork(Thread *t) {
+    schedLock();
+
+    pid_t pid = processCreate();
+    if(!pid) {
+        schedRelease();
+        return -1;
+    }
+
+    KDEBUG("forking parent PID %d into child %d\n", t->pid, pid);
+
+    // we now have a blank slate process, so we need to create a thread in it
+    // and deep clone the parent into the child
+    Process *p = getProcess(pid);
+    p->threadCount = 1;
+    p->threads = calloc(p->threadCount, sizeof(Thread *));
+    if(!p->threads) {
+        free(p);
+        schedRelease();
+        return -1;
+    }
+
+    p->threads[0] = calloc(1, sizeof(Thread));
+    if(!p->threads[0]) {
+        free(p->threads);
+        free(p);
+        schedRelease();
+        return -1;
+    }
+
+    // add the new thread to the queue
+    p->threads[0]->status = THREAD_QUEUED;
+    p->threads[0]->next = NULL;
+    p->threads[0]->pid = pid;
+    p->threads[0]->tid = pid;
+    p->threads[0]->context = calloc(1, PLATFORM_CONTEXT_SIZE);
+    p->threads[0]->time = schedTimeslice(p->threads[0], p->threads[0]->priority);
+    if(!p->threads[0]->context) {
+        free(p->threads[0]);
+        free(p->threads);
+        free(p);
+        schedRelease();
+        return -1;
+    }
+
+    // and clone the parent's context
+    if(!platformCloneContext(p->threads[0]->context, t->context)) {
+        free(p->threads[0]->context);
+        free(p->threads[0]);
+        free(p->threads);
+        free(p);
+        schedRelease();
+        return -1;
+    }
+
+    // and we're done - return zero to the child
+    platformSetContextStatus(p->threads[0]->context, 0);
+    schedRelease();
+    return pid;     // and PID to the parent
+}
