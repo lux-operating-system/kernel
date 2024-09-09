99a1c3f82bbd055045a85597c82a2ccfa6f21705
jewelcodes <omarmelghoul01@gmail.com>
2024-09-09 14:37:38 -0400

    main: open kernel socket

--- src/main.c
+++ src/main.c
@@ -5,23 +5,54 @@
  * Core Microkernel
  */
 
+#include <string.h>
 #include <stdlib.h>
 #include <kernel/logger.h>
 #include <kernel/socket.h>
 #include <kernel/sched.h>
 #include <kernel/modules.h>
 #include <kernel/memory.h>
+#include <kernel/servers.h>
 #include <platform/platform.h>
 
 void *idleThread(void *args) {
     for(;;) {
-        if(!syscallProcess()) {
-            platformHalt();     // only halt the cpu if there's nothing to do
-        }
+        if(!syscallProcess()) platformHalt();
     }
 }
 
 void *kernelThread(void *args) {
+    // open the kernel socket for server communication
+    struct sockaddr_un addr;
+    addr.sun_family = AF_UNIX;
+    strcpy(addr.sun_path, SERVER_KERNEL_PATH);     // this is a special path and not a true file
+
+    int sd = socket(NULL, AF_UNIX, SOCK_DGRAM | SOCK_NONBLOCK, 0);
+    if(sd < 0) {
+        KERROR("failed to open kernel socket: error code %d\n", -1*sd);
+        while(1) platformHalt();
+    }
+
+    int status = bind(NULL, sd, (const struct sockaddr *)&addr, sizeof(struct sockaddr_un));
+    if(status) {
+        KERROR("failed to bind kernel socket: error code %d\n", -1*status);
+        while(1) platformHalt();
+    }
+
+    status = listen(NULL, sd, 2048);
+    if(status) {
+        KERROR("failed to listen to kernel socket: error code %d\n", -1*status);
+        while(1) platformHalt();
+    }
+
+    KDEBUG("kernel is listening on socket %d: %s\n", sd, addr.sun_path);
+
+    void *buffer = malloc(SERVER_MAX_SIZE);
+    if(!buffer) {
+        KERROR("failed to allocate memory for server buffer\n");
+        while(1) platformHalt();
+    }
+
     KDEBUG("attempt to load lumen from ramdisk...\n");
 
     // spawn the router in user space
@@ -55,9 +86,11 @@ void *kernelThread(void *args) {
 
     PhysicalMemoryStatus ps;
     pmmStatus(&ps);
-    KDEBUG("early boot complete, memory usage: %d of %d MiB used\n", ps.usedPages>>8, ps.usablePages>>8);
+    KDEBUG("early boot complete, memory usage: %d MiB / %d MiB\n", ps.usedPages>>8, ps.usablePages>>8);
 
-    return idleThread(args);
+    for(;;) {
+        if(!syscallProcess()) platformHalt();
+    }
 }
 
 // the true kernel entry point is called after platform-specific initialization
@@ -74,14 +107,11 @@ int main(int argc, char **argv) {
     // number of kernel threads = number of CPU cores
     kthreadCreate(&kernelThread, NULL);
 
-    for(int i = 1; i < platformCountCPU(); i++) {
+    for(int i = 1; i < platformCountCPU(); i++)
         kthreadCreate(&idleThread, NULL);
-    }
 
     // now enable the scheduler
     setScheduling(true);
 
-    while(1) {
-        platformHalt();
-    }
+    while(1) platformHalt();
 }
