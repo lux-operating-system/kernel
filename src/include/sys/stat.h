8e06f71e50bf23cd5a60945540e822c52acbd1e6
jewelcodes <omarmelghoul01@gmail.com>
2024-09-11 12:55:15 -0400

    sys: added stat structure

---
+++ src/include/sys/stat.h
@@ -0,0 +1,24 @@
+/*
+ * lux - a lightweight unix-like operating system
+ * Omar Elghoul, 2024
+ * 
+ * Core Microkernel
+ */
+
+#pragma once
+
+#include <sys/types.h>
+
+struct stat {
+    dev_t st_dev;
+    ino_t st_ino;
+    mode_t st_mode;
+    nlink_t st_nlink;
+    uid_t st_uid;
+    gid_t st_gid;
+    dev_t st_rdev;
+    off_t st_size;
+    time_t st_atime, st_mtime, st_ctime;
+    blksize_t st_blksize;
+    blkcnt_t st_blocks;
+};
