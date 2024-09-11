f6e4d0bf3d8dcb0c96a287e76c16505fcf3d8512
jewelcodes <omarmelghoul01@gmail.com>
2024-09-11 12:55:27 -0400

    file: stub for file system call wrappers

---
+++ src/file.c
@@ -0,0 +1,23 @@
+/*
+ * lux - a lightweight unix-like operating system
+ * Omar Elghoul, 2024
+ * 
+ * Core Microkernel
+ */
+
+/* Wrappers for file I/O functions */
+/* None of these functions actually do anything because the microkernel has no
+ * concept of files; these functions just relay the calls to lumen and request
+ * a user space server to fulfill their requests */
+
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+#include <kernel/file.h>
+#include <kernel/io.h>
+#include <kernel/sched.h>
+
+int mount(Thread *t, const char *src, const char *tgt, const char *type, int flags) {
+    /* stub */
+    return -1;
+}
---
+++ src/include/kernel/file.h
@@ -0,0 +1,27 @@
+/*
+ * lux - a lightweight unix-like operating system
+ * Omar Elghoul, 2024
+ * 
+ * Core Microkernel
+ */
+
+#pragma once
+
+#include <stdint.h>
+#include <stddef.h>
+#include <stdbool.h>
+#include <kernel/sched.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+/* system-wide limits */
+#define MAX_FILE                (1 << 18)   // 262k
+#define MAX_FILE_PATH           2048
+
+/* file-specific I/O descriptor (see io.h) */
+typedef struct {
+    Process *process;
+    char path[MAX_FILE_PATH];
+    size_t position;
+    struct stat info;
+} FileDescriptor;
