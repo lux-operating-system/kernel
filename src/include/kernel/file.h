724ada0e447ab0f5a0568513247cbe9cc8d4f9c6
jewelcodes <omarmelghoul01@gmail.com>
2024-09-16 21:28:30 -0400

    file: prototypes for file system calls

--- src/include/kernel/file.h
+++ src/include/kernel/file.h
@@ -27,4 +27,8 @@ typedef struct {
 } FileDescriptor;
 
 /* file system syscalls */
+int open(Thread *, const char *, int, mode_t);
+int close(Thread *, int);
+ssize_t read(Thread *, int, void *, size_t);
+ssize_t write(Thread *, int, const void *, size_t);
 int mount(Thread *, uint64_t, const char *, const char *, const char *, int);
