7e4744eb874dd45df0fd8d1f6d370c9319231b42
jewelcodes <omarmelghoul01@gmail.com>
2024-09-17 18:16:47 -0400

    file: file syscall prototypes

--- src/include/kernel/file.h
+++ src/include/kernel/file.h
@@ -33,3 +33,6 @@ int close(Thread *, uint64_t, int);
 ssize_t read(Thread *, uint64_t, int, void *, size_t);
 ssize_t write(Thread *, uint64_t, int, const void *, size_t);
 int mount(Thread *, uint64_t, const char *, const char *, const char *, int);
+
+ssize_t readFile(Thread *, uint64_t, IODescriptor *, void *, size_t);
+ssize_t writeFile(Thread *, uint64_t, IODescriptor *, const void *, size_t);
