fefc29b927b9c7fcf5d51d173de6e99b4bd6df77
jewelcodes <omarmelghoul01@gmail.com>
2024-09-16 21:26:20 -0400

    io: file descriptor flags

--- src/include/kernel/io.h
+++ src/include/kernel/io.h
@@ -28,6 +28,17 @@
 #define O_NONBLOCK              0x0001
 #define O_NDELAY                O_NONBLOCK
 #define O_CLOEXEC               0x0002
+#define O_RDONLY                0x0004
+#define O_WRONLY                0x0008
+#define O_RDWR                  (O_RDONLY | O_WRONLY)
+#define O_APPEND                0x0010
+#define O_CREAT                 0x0020
+#define O_DSYNC                 0x0040
+#define O_EXCL                  0x0080
+#define O_NOCTTY                0x0100
+#define O_RSYNC                 0x0200
+#define O_SYNC                  0x0400
+#define O_TRUNC                 0x0800
 
 // TODO: decide whether to implement named pipes as files or an independent type
 
