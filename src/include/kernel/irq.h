029ebe1a3164dd4c84726f052cfc59b89eb62566
jewelcodes <omarmelghoul01@gmail.com>
2024-09-19 18:02:51 -0400

    irq: restructure IRQ handlers to allow user space handling

--- src/include/kernel/irq.h
+++ src/include/kernel/irq.h
@@ -12,10 +12,10 @@
 
 /* Generic platform-independent structure for IRQ handlers */
 typedef struct {
-    char name[256];     // device/driver name
+    char name[256];     // device name
+    char driver[256];   // name of server/driver prefixed with "lux://ks"
     int kernel;         // 1 for kernel-level IRQ handler, 0 for user space
     uintptr_t khandler; // entry point for kernel IRQ handlers
-    int socket;         // socket for user space driver
     int level, high;    // trigger mode
 } IRQHandler;
 
