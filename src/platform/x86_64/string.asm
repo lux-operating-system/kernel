e57fb4c535dd4a443d64c1690671220d398858ad
jewelcodes <omarmelghoul01@gmail.com>
2024-09-18 23:43:44 -0400

    x86_64: implemented memcpy() in asm for optimization

---
+++ src/platform/x86_64/string.asm
@@ -0,0 +1,28 @@
+
+; lux - a lightweight unix-like operating system
+; Omar Elghoul, 2024
+
+[bits 64]
+
+section .text
+
+; memcpy(void *dst, const void *src, size_t n)
+global memcpy
+align 16
+memcpy:
+    mov rax, rdi
+
+    cmp rdx, 8
+    jl .low
+
+    mov rcx, rdx
+    shr rcx, 3          ; div 8
+    rep movsq
+
+    and rdx, 7          ; mod 8
+
+.low:
+    mov rcx, rdx
+    rep movsb
+
+    ret
