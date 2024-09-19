1a2acda43702328162281ee55dfc37d74b8270a4
jewelcodes <omarmelghoul01@gmail.com>
2024-09-18 23:52:01 -0400

    x86_64: implemented memset() in asm

--- src/platform/x86_64/string.asm
+++ src/platform/x86_64/string.asm
@@ -26,3 +26,38 @@ memcpy:
     rep movsb
 
     ret
+
+; void *memset(void *dst, int val, size_t n)
+global memset
+align 16
+memset:
+    mov r8, rdx         ; r8 = count
+
+    and rsi, 0xFF
+    mov rax, rsi        ; low 8 bits
+
+    cmp rdx, 8
+    jl .low
+
+    shr rax, 8
+    or rax, rsi         ; 16 bits
+    mov si, ax
+    shr rax, 16
+    mov ax, si          ; 32 bits
+    mov esi, eax
+    shr rax, 32
+    mov eax, esi        ; full 64 bits
+
+    mov rcx, rdx
+    shr rcx, 3          ; div 8
+    rep stosq
+
+    and rdx, 7          ; mod 8
+
+.low:
+    mov rcx, rdx
+    rep stosb
+
+    mov rax, rdi
+    sub rax, r8
+    ret
