9966c3c0ad99dce38afbec2d3797cb31ce121203
jewelcodes <omarmelghoul01@gmail.com>
2024-09-25 15:42:01 -0400

    types: fixed off_t

--- src/include/sys/types.h
+++ src/include/sys/types.h
@@ -19,7 +19,7 @@ typedef uint64_t nlink_t;
 typedef uint32_t uid_t;
 typedef uint32_t gid_t;
 typedef int32_t pid_t;
-typedef uint64_t off_t;
+typedef int64_t off_t;
 typedef uint64_t time_t;
 typedef uint16_t blksize_t;
 typedef uint64_t blkcnt_t;
