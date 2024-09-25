52c12fb29c588cd14206249869c59235eea160c9
jewelcodes <omarmelghoul01@gmail.com>
2024-09-25 16:09:37 -0400

    syscalls: updated syscall dispatch table

--- src/include/kernel/syscalls.h
+++ src/include/kernel/syscalls.h
@@ -11,12 +11,12 @@
 #include <stdbool.h>
 #include <kernel/sched.h>
 
-#define MAX_SYSCALL             49
+#define MAX_SYSCALL             51
 
 /* IPC syscall indexes, these will be used for immediate handling without
  * waiting for the queue */
-#define SYSCALL_IPC_START       39      // recv()
-#define SYSCALL_IPC_END         40      // send()
+#define SYSCALL_IPC_START       43      // recv()
+#define SYSCALL_IPC_END         44      // send()
 
 #define SYSCALL_RW_START        16      // read()
 #define SYSCALL_RW_END          17      // write()
