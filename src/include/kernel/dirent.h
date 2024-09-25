6a88e4de0c08b634ed4b2a2cc07501a4568c5b0b
jewelcodes <omarmelghoul01@gmail.com>
2024-09-25 15:52:45 -0400

    dirent: added prototypes

--- src/include/kernel/dirent.h
+++ src/include/kernel/dirent.h
@@ -8,8 +8,23 @@
 #pragma once
 
 #include <sys/types.h>
+#include <kernel/sched.h>
+#include <kernel/file.h>
 
-typedef struct {
+typedef int DIR;
+
+struct dirent {
     ino_t d_ino;
     char d_name[];
-} dirent;
+};
+
+/* directory-specific I/O descriptor (see io.h) */
+typedef struct {
+    Process *process;
+    char path[MAX_FILE_PATH];
+    size_t position;
+} DirectoryDescriptor;
+
+int opendir(Thread *, uint64_t, const char *);
+int closedir(Thread *, DIR *);
+int readdir_r(Thread *, uint64_t, DIR *, struct dirent *, struct dirent **);
