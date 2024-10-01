2e9fc4611208ad4703878604a765ed437e7cdacc
jewelcodes <omarmelghoul01@gmail.com>
2024-10-01 14:03:54 -0400

    libc: added limits.h

---
+++ src/include/limits.h
@@ -0,0 +1,11 @@
+/*
+ * lux - a lightweight unix-like operating system
+ * Omar Elghoul, 2024
+ * 
+ * Kernel-Level Partial Implementation of the C Library
+ */
+
+#pragma once
+
+#define MAX_PATH            2048
+#define ARG_MAX             128
