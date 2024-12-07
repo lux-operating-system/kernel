74c68255feb35a12837514bc2a7ec3f3919a5f1f
jewelcodes <omarmelghoul01@gmail.com>
2024-12-06 23:45:51 -0500

    x86_64: fix memmove

--- src/platform/x86_64/string.asm
+++ src/platform/x86_64/string.asm
@@ -34,7 +34,7 @@ memmove:
     mov rax, rdi        ; return value
 
     mov r8, rdi
-    sub r8, rsi
+    sub r8, rsi         ; dst-src
     jns .check_overlap
 
     not r8
@@ -42,9 +42,22 @@ memmove:
 
 .check_overlap:
     cmp r8, 8
-    jle memcpy
+    jge memcpy
 
+    js .backward        ; if dst<src copy backwards
+
+.forward:
     mov rcx, rdx
+    rep movsb
+
+    ret
+
+.backward:
+    std
+    mov rcx, rdx
+    dec rdx
+    add rdi, rdx
+    add rsi, rdx
     rep movsb
 
     ret
