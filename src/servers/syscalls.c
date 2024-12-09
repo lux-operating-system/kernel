2d1f5b2eb40f916961e1547a18dca8dbb3da67a0
jewelcodes <omarmelghoul01@gmail.com>
2024-12-09 01:10:23 -0500

    servers: make external server ID checks more lenient

--- src/servers/syscalls.c
+++ src/servers/syscalls.c
@@ -20,18 +20,7 @@
 
 void handleSyscallResponse(int sd, const SyscallHeader *hdr) {
     SyscallRequest *req = getSyscall(hdr->header.requester);
-    if(!req) {
-        KWARN("received response for syscall 0x%X pid %d but no such request exists\n", hdr->header.command, hdr->header.requester);
-        return;
-    }
-
-    if(req->requestID != hdr->id) {
-        KWARN("received response for syscall 0x%X (kernel syscall %d) pid %d but IDs mismatch; terminating thread\n", hdr->header.command, req->function, hdr->header.requester);
-        schedLock();
-        terminateThread(req->thread, -1, false);
-        schedRelease();
-        return;
-    }
+    if(!req || req->requestID != hdr->id) return;
 
     // default action is to unblock the thread
     Process *p = getProcess(req->thread->pid);
