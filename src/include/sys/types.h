cd33b2b1014d35b2e25ed1d062c50834c13a4cf3
jewelcodes <omarmelghoul01@gmail.com>
2025-01-04 18:25:07 -0500

    types: defined id_t and made clock types signed

--- src/include/sys/types.h
+++ src/include/sys/types.h
@@ -16,11 +16,15 @@ typedef uint64_t dev_t;     // device
 typedef uint64_t ino_t;     // inode
 typedef uint32_t mode_t;
 typedef uint64_t nlink_t;
-typedef uint32_t uid_t;
-typedef uint32_t gid_t;
-typedef int32_t pid_t;
+typedef int32_t id_t;
+typedef id_t uid_t;
+typedef id_t gid_t;
+typedef id_t pid_t;
 typedef int64_t off_t;
-typedef uint64_t time_t;
+typedef int64_t time_t;
+typedef int64_t clock_t;
+typedef int64_t suseconds_t;
+typedef suseconds_t useconds_t;
 typedef uint16_t blksize_t;
 typedef uint64_t blkcnt_t;
 typedef int64_t ssize_t;
