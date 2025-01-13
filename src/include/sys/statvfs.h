7524ad59a411ef6db9a08e6f64409a7423534796
jewelcodes <omarmelghoul01@gmail.com>
2025-01-13 01:00:22 -0500

    statvfs: structure and prototypes for statvfs() and fstatvfs()

---
+++ src/include/sys/statvfs.h
@@ -0,0 +1,31 @@
+/*
+ * lux - a lightweight unix-like operating system
+ * Omar Elghoul, 2024
+ * 
+ * Core Microkernel
+ */
+
+#pragma once
+
+#include <sys/types.h>
+#include <kernel/sched.h>
+
+#define ST_RDONLY           0x01
+#define ST_NOSUID           0x02
+
+struct statvfs {
+    unsigned long f_bsize;
+    unsigned long f_frsize;
+    fsblkcnt_t f_blocks;
+    fsblkcnt_t f_bfree;
+    fsblkcnt_t f_bavail;
+    fsfilcnt_t f_files;
+    fsfilcnt_t f_ffree;
+    fsfilcnt_t f_favail;
+    unsigned long f_sid;
+    unsigned long f_flag;
+    unsigned long f_namemax;
+};
+
+int fstatvfs(Thread *, uint64_t, int, struct statvfs *);
+int statvfs(Thread *, uint64_t, const char *, struct statvfs *);
