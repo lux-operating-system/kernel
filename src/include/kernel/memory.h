e3136354de4f990f4d37c68ce26e8fb4c8db9581
jewelcodes <omarmelghoul01@gmail.com>
2025-01-21 14:43:58 -0500

    mm: declared MAP_ANONYMOUS and MAP_ANON

--- src/include/kernel/memory.h
+++ src/include/kernel/memory.h
@@ -51,6 +51,8 @@
 #define MAP_SHARED              0x01
 #define MAP_PRIVATE             0x02
 #define MAP_FIXED               0x04
+#define MAP_ANONYMOUS           0x08
+#define MAP_ANON                (MAP_ANONYMOUS)
 
 #define MS_ASYNC                0x01
 #define MS_SYNC                 0x02
